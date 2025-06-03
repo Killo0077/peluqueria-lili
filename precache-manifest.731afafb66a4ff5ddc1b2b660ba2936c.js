dd32e7d332de9b658d96b26b08f12cc29519e4f4
Killo0077 <pablobaenav@gmail.com>
2025-06-03 20:43:00 +0100

    Updates

---
+++ precache-manifest.731afafb66a4ff5ddc1b2b660ba2936c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fb94cc988a9d8686712c3ed0760eb1",
+    "url": "/peluqueria-lili/index.html"
+  },
+  {
+    "revision": "aa4a5d983343e1d9187e",
+    "url": "/peluqueria-lili/static/css/main.a252363d.chunk.css"
+  },
+  {
+    "revision": "578f6fa4132559d6e4ec",
+    "url": "/peluqueria-lili/static/js/2.1ed2d477.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/peluqueria-lili/static/js/2.1ed2d477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4a5d983343e1d9187e",
+    "url": "/peluqueria-lili/static/js/main.7d660370.chunk.js"
+  },
+  {
+    "revision": "5c7fa177eb530be80f26",
+    "url": "/peluqueria-lili/static/js/runtime-main.c75f2d60.js"
+  }
+]);
